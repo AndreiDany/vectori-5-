871337e9c0ef31f9d19d621e3bc4c06b0440fbd5
Daniel Leon Andrei <daniel.andrei@emmanuel.ro>
2021-11-25 23:12:07 +0200

    Hello

--- vectori(5)/Program.cs
+++ vectori(5)/Program.cs
@@ -6,7 +6,23 @@ namespace vectori_5_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] v = new int[100];
+            int n;
+            int[] frequencies = new int[100];
+            n = Int32.Parse(Console.ReadLine());
+            int maxx = int.MinValue;
+            int minn = int.MaxValue;
+            for (int i = 1; i <= n; i++)
+            {
+                v[i] = Int32.Parse(Console.ReadLine());
+                frequencies[v[i]]++;
+                if (v[i] > maxx) maxx = v[i];
+                if (v[i] < minn) minn = v[i];
+            }
+            for (int i = minn; i <= maxx; i++)
+            {
+                Console.WriteLine(frequencies[i]);
+            }
         }
     }
 }
